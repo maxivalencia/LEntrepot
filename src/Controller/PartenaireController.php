e1a7ec8dc22f37706f1bd0c9f93ec8931d3b7636
kaira <husaynmozart@gmail.com>
2021-09-01 13:28:09 +0300

    partenaire

--- src/Controller/PartenaireController.php
+++ src/Controller/PartenaireController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
 
 /**
  * @Route("/partenaire")
@@ -18,10 +19,15 @@ class PartenaireController extends AbstractController
     /**
      * @Route("/", name="partenaire_index", methods={"GET"})
      */
-    public function index(PartenaireRepository $partenaireRepository): Response
+    public function index(PartenaireRepository $partenaireRepository, Request $request, PaginatorInterface $paginator): Response
     {
+        $pagination = $paginator->paginate(
+            $partenaireRepository->findBy([], ["id" => "DESC"]), /* query NOT result */
+            $request->query->getInt('page', 1)/*page number*/,
+            20/*limit per page*/
+        );
         return $this->render('partenaire/index.html.twig', [
-            'partenaires' => $partenaireRepository->findAll(),
+            'partenaires' => $pagination,
         ]);
     }
 
